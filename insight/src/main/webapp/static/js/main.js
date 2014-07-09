dfd776c8cf8c40921a7a95f89272f02e23c7c20c
jdonee <jdonee.zeng@gmail.com>
2014-07-09 23:05:48 +0800

    1、修复各种bug.

--- insight/src/main/webapp/static/js/main.js
+++ insight/src/main/webapp/static/js/main.js
@@ -1,1 +1,22 @@
+//所有特定脚本:浏览器或者视窗
+Modernizr.load([
+    {//让IE8及以下支持支持media特性
+    	test:Modernizr.mq('only all'),
+    	nope:[_ctx+'/static/js/respond/respond.min.js',
+    	      _ctx+'/static/js/respond/respond.matchmedia.addListener.min.js']
+	}
+    ,{//让IE9及以下支持填充文本
+		test: Modernizr.input.placeholder,
+		nope: [_ctx+'/static/js/jquery-placeholder/jquery.placeholder.css',
+		       _ctx+'/static/js/jquery-placeholder/jquery.placeholder.js'],
+		complete:function(){
+			$(document).ready(function(){
+				if(!Modernizr.input.placeholder){
+					$('input, textarea').placeholder();
+				}
+			});
+		}
+	}
+]);
+
 
